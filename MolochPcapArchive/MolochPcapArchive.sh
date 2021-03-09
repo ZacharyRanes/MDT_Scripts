34395c94dacaba4f67c1b1207ccb9ca12a6ca682
zacharyranes <zachary.ranes@outlook.com>
2021-03-08 22:15:31 -0500

    Update with cleaner code and more output

--- MolochPcapArchive/MolochPcapArchive.sh
+++ MolochPcapArchive/MolochPcapArchive.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#If the folder the Moloch is storing pcap is not standard 3.2 change these vars
+#Vars may need to be changed, defaults for CVA/H 3.2.1 and 89th custom configs
 pcapPath="/data/moloch/raw"
 archivePath="/data/moloch/raw/archive"
+age=3 #days
 
-#loops through all moloch capture pods
-#awk expration may need to change if pod naming scheme is not standard
+echo -n "Started: " > last_run.txt
+date >> last_run.txt
+
 for podName in $(kubectl get pods | awk '/-moloch/{print$1;}');
 do
-#kubectl is called two times because the output from the first is used for input of second call
-kubectl exec $podName -- bash -c "\
-cd $pcapPath && \
-find *.pcap -type f -mtime +15 -print" | while read fileName;
-do
-kubectl exec $podName -- bash -c "\
-cd $pcapPath && \
-mv $fileName $archivePath/$fileName && \
-/data/moloch/db/db.pl elaticsearch:9200 mv $pcapPath/$fileName $archivePath/$fileName"
-#above two lines are where all the magic happens
+    kubectl exec $podName -- bash -c "cd $pcapPath && find *.pcap -type f - mtime +$age -print" | while read fileName;
+    do
+        echo -n "moving $fileName"
+        kubectl exec $podName -- bash -c "mv $pcapPath/$fileName $archivePath/$fileName"
+        echo "done"
+    done;
 done;
-done;
+
+echo -n "Finished: " >> last_run.txt
+date >> last_run.txt
